361d7cc921e0834c7ac8762d710ec0bbcaac311f
sa-webb <sawebb30@gmail.com>
2022-01-07 14:42:33 -0500

    initial: environment

---
+++ src/global/index.ts
@@ -0,0 +1,10 @@
+import Environment from "../environments/environment";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var environment: Environment;
+}
+
+export const setGlobalEnvironment = (environment: Environment): void => {
+  global.environment = environment;
+};
